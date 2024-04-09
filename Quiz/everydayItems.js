dca8574cb445c2016a39030125fdd2aa2f18e770
izzyheng <iheng@andrew.cmu.edu>
2024-04-09 19:06:45 -0400

    first round

---
+++ Quiz/everydayItems.js
@@ -0,0 +1,170 @@
+let quizData = [
+    {
+      question: "Which one of these is bed?",
+      image: "purple.jpg",
+      options: ["보라색", "까만색", "하얀색", "노란색"],
+      correct: "보라색",
+    },
+    {
+      question: "Which one of these is smartphone?",
+      image: "black.jpg",
+      options: ["보라색", "까만색", "하얀색", "노란색"],
+      correct: "까만색",
+    },
+    {
+      question: "Which one of these is car?",
+      image: "white.jpg",
+      options: ["보라색", "까만색", "하얀색", "노란색"],
+      correct: "하얀색",
+    },
+    {
+      question: "Which one of these is book?",
+      image: "yellow.jpg",
+      options: ["보라색", "까만색", "하얀색", "노란색"],
+      correct: "노란색",
+    },
+  ];
+  
+  const quizContainer = document.querySelector(".quiz-container");
+  const question = document.querySelector(".quiz-container .question");
+  const image = document.querySelector(".quiz-container #imgSource");
+  const options = document.querySelector(".quiz-container .options");
+  const nextBtn = document.querySelector(".quiz-container .next-btn");
+  const quizResult = document.querySelector(".quiz-result");
+  const startBtnContainer = document.querySelector(".start-btn-container");
+  const startBtn = document.querySelector(".start-btn-container .games .start-btn2");
+  
+  let questionNumber = 0;
+  let score = 0;
+  const MAX_QUESTIONS = 4;
+  
+  const shuffleArray = (array) => {
+    return array.slice().sort(() => Math.random() - 0.5);
+  };
+  
+  quizData = shuffleArray(quizData);
+  
+  const resetLocalStorage = () => {
+    for (i = 0; i < MAX_QUESTIONS; i++) {
+      localStorage.removeItem(`userAnswer_${i}`);
+    }
+  };
+  
+  resetLocalStorage();
+  
+  const checkAnswer = (e) => {
+    let userAnswer = e.target.textContent;
+    if (userAnswer === quizData[questionNumber].correct) {
+      score++;
+      e.target.classList.add("correct");
+    } else {
+      e.target.classList.add("incorrect");
+    }
+  
+    localStorage.setItem(`userAnswer_${questionNumber}`, userAnswer);
+  
+    let allOptions = document.querySelectorAll(".quiz-container .option");
+    allOptions.forEach((o) => {
+      o.classList.add("disabled");
+    });
+  };
+  
+  const createQuestion = () => {
+  
+    options.innerHTML = "";
+    image.src = '/assets/' + quizData[questionNumber].image;
+    question.innerHTML = `<span class='question-number'>${
+      questionNumber + 1
+    }/${MAX_QUESTIONS}</span>${quizData[questionNumber].question}`;
+  
+    const shuffledOptions = shuffleArray(quizData[questionNumber].options);
+  
+    shuffledOptions.forEach((o) => {
+      const option = document.createElement("button");
+      option.classList.add("option");
+      option.innerHTML = o;
+      option.addEventListener("click", (e) => {
+        checkAnswer(e);
+      });
+      options.appendChild(option);
+    });
+  };
+  
+  const retakeQuiz = () => {
+    questionNumber = 0;
+    score = 0;
+    quizData = shuffleArray(quizData);
+    resetLocalStorage();
+  
+    createQuestion();
+    quizResult.style.display = "none";
+    quizContainer.style.display = "block";
+  };
+  
+  const displayQuizResult = () => {
+    quizResult.style.display = "flex";
+    quizContainer.style.display = "none";
+    quizResult.innerHTML = "";
+  
+    const resultHeading = document.createElement("h2");
+    resultHeading.innerHTML = `You have scored ${score} out of ${MAX_QUESTIONS}.`;
+    quizResult.appendChild(resultHeading);
+  
+    for (let i = 0; i < MAX_QUESTIONS; i++) {
+      const resultItem = document.createElement("div");
+      resultItem.classList.add("question-container");
+  
+      const userAnswer = localStorage.getItem(`userAnswer_${i}`);
+      const correctAnswer = quizData[i].correct;
+  
+      let answeredCorrectly = userAnswer === correctAnswer;
+  
+      if (!answeredCorrectly) {
+        resultItem.classList.add("incorrect");
+      }
+  
+      resultItem.innerHTML = `<div class="question">Question ${i + 1}: ${
+        quizData[i].question
+      }</div>
+      <div class="user-answer">Your answer: ${userAnswer || "Not Answered"}</div>
+      <div class="correct-answer">Correct answer: ${correctAnswer}</div>`;
+  
+      quizResult.appendChild(resultItem);
+    }
+  
+    // const retakeBtn = document.createElement("button");
+    // retakeBtn.classList.add("retake-btn");
+    // retakeBtn.innerHTML = "Retake Quiz";
+    // retakeBtn.addEventListener("click", retakeQuiz);
+    // quizResult.appendChild(retakeBtn);
+
+
+
+    const retakeBtn = document.createElement("button");
+    retakeBtn.classList.add("retake-btn");
+    retakeBtn.innerHTML = "Retake Quiz";
+    retakeBtn.addEventListener("click", () => {
+        resetLocalStorage();
+        quizResult.style.display = "none";
+        startBtnContainer.style.display = "block";
+    });
+    quizResult.appendChild(retakeBtn);
+  };
+  
+  const displayNextQuestion = () => {
+    if (questionNumber >= MAX_QUESTIONS - 1) {
+      displayQuizResult();
+      return;
+    }
+  
+    questionNumber++;
+    createQuestion();
+  };
+  
+  nextBtn.addEventListener("click", displayNextQuestion);
+  
+  startBtn.addEventListener("click", () => {
+    startBtnContainer.style.display = "none";
+    quizContainer.style.display = "block";
+    retakeQuiz();
+  });
